cf102eb45bea3358eab37ac2b688e132186a95d8
VisikoW <aionconts@hotmail.com>
2022-05-14 16:02:52 -0300

    dia bastente produtivo

---
+++ Day 16/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+#criando a tartaruga
+vinycius = Turtle()
+vinycius.shape("turtle")
+vinycius.speed(1)
+vinycius.shapesize(2)
+vinycius.forward(100)
+
+windows = Screen()
+
+
+
+windows.exitonclick()
+windows.mainloop()
