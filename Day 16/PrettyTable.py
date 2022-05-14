459ea9bcc2c280356f8f3667b17f7de7ad7fdf52
VisikoW <aionconts@hotmail.com>
2022-05-14 18:48:09 -0300

    Correção de bug

--- Day 16/PrettyTable.py
+++ Day 16/PrettyTable.py
@@ -6,7 +6,6 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire",])
 
-table.add_row()
 
 print(table)
 
