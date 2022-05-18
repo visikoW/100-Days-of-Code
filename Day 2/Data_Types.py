9db6c357effe97841d92489da9bcebf28a55b31e
VisikoW <aionconts@hotmail.com>
2022-05-17 22:59:23 -0300

    Error

--- Day 2/Data_Types.py
+++ Day 2/Data_Types.py
@@ -1,4 +1,9 @@
 # Data Types
 
-    # strings
+# strings
 print("Hello, World!"[4])
+"""
+Those strings are an set of letter, this's
+an set of caracters, the program read letter by letter to get
+full word.
+"""
