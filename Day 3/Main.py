5a01efb07a3105570018af4264582bd460644ea9
VisikoW <aionconts@hotmail.com>
2022-05-28 20:31:58 -0300

    Day 4

---
+++ Day 3/Main.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+from math import ceil
+
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height = float(height)
+weight = float(weight)
+BMI = ceil(weight/(height**2))
+
+if BMI <= 18.5:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI <= 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI <= 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI <= 35:
+    print(f"Your BMI is {BMI}, you are obese.")
+elif BMI > 35:
+    print(f"Your BMI is {BMI}, you are clinically obese.")
+
