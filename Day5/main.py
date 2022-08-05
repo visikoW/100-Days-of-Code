a49c4ffb1693a9bb829dec8baaf1a5b6cb3a1975
visiko <a.pandagok1@gmail.com>
2022-08-05 16:15:00 -0300

    Removendo o Day5

--- Day5/main.py
+++
@@ -1,6 +0,0 @@
-employee_name = input('Digite o seu nome por favor: ')
-explict_employee_name = input('O seu nome é {} (S/n)? '.format(employee_name))
-if explict_employee_name == 'S':
-    print('\nSeja muito bem vindo {}!\n'.format(employee_name))
-else:
-    print('Por favor, digite novamente {}!'.format(employee_name))
