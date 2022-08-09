eccb3e9d88db2b19931c750c23c511709e5eff50
visiko <a.pandagok1@gmail.com>
2022-08-09 17:23:52 -0300

    Envio

--- Day 8/Exercise4_part4.py
+++ Day 8/Exercise4_part4.py
@@ -3,39 +3,39 @@ numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbol = ['!', '?', '*', '%', '&', '#', '$', '@']
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    selected = char
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        #TODO-3: What happens if the user enters a number/symbol/space?
+        #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+        #e.g. start_text = "meet me at 3"
+        #end_text = "•••• •• •• 3"
+        selected = char
 
-    # To alphabetic characters only
-    if char in alphabet:
-        selected = alphabet
-    
-    # To number characters only
-    elif char in numbers:
-        selected = numbers
+        # To alphabetic characters only
+        if char in alphabet:
+            selected = alphabet
+        
+        # To number characters only
+        elif char in numbers:
+            selected = numbers
 
-    # To symbol characters only
-    elif char in symbol:
-        selected = symbol
+        # To symbol characters only
+        elif char in symbol:
+            selected = symbol
 
-    # For every variety
-    position = selected.index(char)
-    new_position = position + shift_amount
-    if char not in alphabet and char not in numbers and char not in symbol:
-        new_position = 0
+        # For every variety
+        position = selected.index(char)
+        new_position = position + shift_amount
+        if char not in alphabet and char not in numbers and char not in symbol:
+            new_position = 0
 
-    if new_position > len(selected):
-        new_position %= len(selected)
-    end_text += selected[new_position]
+        if new_position > len(selected):
+            new_position %= len(selected)
+        end_text += selected[new_position]
 
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+        print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
 from art import *
