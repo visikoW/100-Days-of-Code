d2775fdac845de03194bbd6c3310a19b1d9f988f
visiko <a.pandagok1@gmail.com>
2022-08-09 11:22:48 -0300

    Muito complicado o melhoramento do part 4

--- Day 8/Exercise4_part4.py
+++ Day 8/Exercise4_part4.py
@@ -1,4 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbol = ['!', '?', '*', '%', '&', '#', '$', '@']
 
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
@@ -9,10 +11,30 @@ def caesar(start_text, shift_amount, cipher_direction):
     #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
     #e.g. start_text = "meet me at 3"
     #end_text = "•••• •• •• 3"
-    position = alphabet.index(char)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
+    selected = char
+
+    # To alphabetic characters only
+    if char in alphabet:
+        selected = alphabet
     
+    # To number characters only
+    elif char in numbers:
+        selected = numbers
+
+    # To symbol characters only
+    elif char in symbol:
+        selected = symbol
+
+    # For every variety
+    position = selected.index(char)
+    new_position = position + shift_amount
+    if char not in alphabet and char not in numbers and char not in symbol:
+        new_position = 0
+
+    if new_position > len(selected):
+        new_position %= len(selected)
+    end_text += selected[new_position]
+
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
@@ -23,18 +45,21 @@ print(logo)
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 #Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+    #Try running the program and entering a shift number of 45.
+    #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+    #Hint: Think about how you can use the modulus (%).
+    if shift > len(alphabet):
+        shift %= len(alphabet)
 
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-#Hint: Think about how you can use the modulus (%).
-if shift > len(alphabet):
-    shift %= len(alphabet)
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-print(shift)
-
-# caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    # Test to continue
+    answer = input("Do you want to restart the cipher program? (y/n): ")
+    if answer.lower() != "y":
+        break
