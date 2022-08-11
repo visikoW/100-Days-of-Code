c02a236255d5678c2eee104ba95caba5af3f3c67
visiko <a.pandagok1@gmail.com>
2022-08-11 12:15:43 -0300

    Start Day9

---
+++ Day 9/Dictionary.py
@@ -0,0 +1,14 @@
+# Pessoas = {{"Nome": "Vinycius Florêncio da Silva Alves", "Idade": "20", "Endereço": "Rua Arcelina Pereira de Souza"},{"Nome": "Anderson Florêncio da Silva ALves", "Idade": "25", "Endereço": "Rua Arcelina Pereira de Souza"}}
+Pessoas = {"Nome": "Vinycius Florêncio da Silva Alves", "Idade": "20", "Endereço": "Rua Arcelina Pereira de Souza"}
+# print(Pessoas, Pessoas.keys())
+
+# Pessoas["Estado Civil"] = "Solteiro"
+
+# print(Pessoas, Pessoas.keys())
+
+# Pessoas["Nome"] += "Anderson Florêncio da Silva Alves"
+
+# print(Pessoas["Nome"])
+
+for x in Pessoas:
+    print(x)
