96807eb3216926359ccdbb2cddd7917e45943d76
visiko <a.pandagok1@gmail.com>
2022-08-23 17:36:17 -0300

    Tragedia

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -30,22 +30,47 @@ def deck(player: list):
     for x in range(0,2):
         add_card(player)
 
-def end_game():
-    # Verify if the player just passed the number of cards necessary to play again.
-    def player_win():
-        print(f"  Your final hand: {player_deck}, final score: {sum(player_deck)}")
-        print(f"  Computer's final hand: {opponent_deck}, final score: {sum(opponent_deck)}")
-        print("You went over. You lose =(")
-    def opponent_win():
-        print(f"  Your final hand: {player_deck}, final score: {sum(player_deck)}")
-        print(f"  The computer's final hand: {opponent_deck}, final score: {sum(opponent_deck)}")
-        print("Opponent went over. You win =)")
+# Verify if the player just passed the number of cards necessary to play again.
+def opponent_win():
+    print(f"  Your final hand: {player_deck}, final score: {sum(player_deck)}")
+    print(f"  Computer's final hand: {opponent_deck}, final score: {sum(opponent_deck)}")
+    print("You went over. You lose =(")
+
+def player_win():
+    print(f"  Your final hand: {player_deck}, final score: {sum(player_deck)}")
+    print(f"  The computer's final hand: {opponent_deck}, final score: {sum(opponent_deck)}")
+    print("Opponent went over. You win =)")
+
+# End Game
+def end_game(player_decision):
+    """Tell if the player or the opponent win"""
+    sum_player = sum(player_deck)
+    sum_opponent = sum(opponent_deck)
+
+    if sum_player == sum_opponent:
+        return print("Drawn")
+    elif sum_player == 21:
+        return player_win()
+    elif sum_player > 21:
+        return opponent_win()
+    elif sum_opponent > 21:
+        return player_win()
+    elif sum_opponent == 21:
+        return opponent_win()
+
+    if player_decision == "y":
+        add_card(player_deck)
+
+    if sum_opponent > sum_player:
+        return opponent_win()
+    elif sum_player > sum_opponent:
+        return player_win()
 
 def start():
     """Start the blackjack game."""
-    # Space and show the logo
-    print("\n" * 50)
+    # Show the logo
     print(logo)
+    print("\n" * 50)
 
     # Set your deck and the opponent deck and start the table
     deck(player_deck)
@@ -54,24 +79,9 @@ def start():
         add_card(opponent_deck)
     table()
 
-    # Play
     while True:
-        # Who will win?
-        # First Try: The player start with the good hand?
-        # Second Try: Your hand overpass the limit?
-        if sum(player_deck) == 21 or sum(player_deck) > 21:
-            end_game()
-            break
-
-        # Third Try: The hand isn't enoght to end the game?
-        elif sum(player_deck) < 21:
-            again = input("Type 'y' to get another card, type 'n' to pass: ")
-            if again == "y":
-                add_card(player_deck)
-                table()
-            else:
-                end_game()
-                break
+        another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        end_game(another_card)
 
 while True:
     confirm = input("Do you want to play a game of blackjack? (y/n): ")
